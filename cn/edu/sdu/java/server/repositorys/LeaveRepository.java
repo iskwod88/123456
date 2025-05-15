8a34b6001463ac9ee3195b4a6a580d87314b75f0
iskwod88 <codetruth1211@gmail.com>
2025-05-15 23:42:38 +0800

    Delete cn directory

--- cn/edu/sdu/java/server/repositorys/LeaveRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.sdu.java.server.repositorys;
-
-import cn.edu.sdu.java.server.models.FamilyMember;
-import cn.edu.sdu.java.server.models.Leave;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeaveRepository extends JpaRepository<Leave,Integer> {
-}
--- cn/edu/sdu/java/server/repositorys/StudentStatisticsRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.sdu.java.server.repositorys;
-
-import cn.edu.sdu.java.server.models.StudentStatistics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentStatisticsRepository extends JpaRepository<StudentStatistics,Integer> {
-}
